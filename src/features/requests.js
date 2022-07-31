fc801f6b2854446d637a5244c3debffec3f12c83
markaeroltomarse <marktomarse@gmail.com>
2022-07-31 23:23:22 +0800

    prod cinfig

--- src/features/requests.js
+++ src/features/requests.js
@@ -1,4 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
-export const userApi = isProd ? 'https://mysql-express-user.vercel.app/' : 'http://localhost:3001/api/user'
+const baseUrl = isProd ? 'https://videoconferencing-mysql.herokuapp.com' : 'http://localhost:3001'
+
+export const userApi = `${baseUrl}/api/user` 
 
