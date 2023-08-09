4f7abe77e15759b7af6639084ef5f3d6bf97d930
markaeroltomarse <marktomarse@gmail.com>
2023-08-09 08:57:38 +0800

    update be dev URL

--- src/features/requests.js
+++ src/features/requests.js
@@ -1,7 +1,9 @@
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === "production";
 
-export const baseUrl = isProd ? 'https://videoconferencing-mysql.herokuapp.com' : 'http://localhost:3001'
+export const baseUrl = isProd
+  ? "https://agora-be.onrender.com"
+  : "http://localhost:3001";
 
-export const userApi = `${baseUrl}/api/user` 
+export const userApi = `${baseUrl}/api/user`;
 
-export const roomApi = `${baseUrl}/api/room`
+export const roomApi = `${baseUrl}/api/room`;
